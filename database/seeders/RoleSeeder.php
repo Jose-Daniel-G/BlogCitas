a2f6d0ac4ad4c5866d3e27be7dd298e514e289fc
Jose-Grijalba <jose.grijalba04@gmail.com>
2024-09-23 08:11:20 -0500

    SEEDERS AND MIGRATIONS

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -16,49 +16,108 @@ class RoleSeeder extends Seeder
      */
     public function run()
     {
-        $role1 = Role::create(['name'=>'Admin']);
-        $role2 = Role::create(['name'=>'Blogger']);
-        $role3 = Role::create(['name'=>'Profesor']);
-        $role4 = Role::create(['name'=>'Alumno']);
+        //-----------------------------------------------------------------------------------------------        
+        $role3 = Role::create(['name' => 'Profesor']);
+        $role4 = Role::create(['name' => 'Alumno']);
+        // ----------------------------------------------------------------------------------------------
+        $admin = Role::create(['name' => 'admin']);
+        $blogger = Role::create(['name' => 'blogger']);
 
-        // Permission::create(['name'=>'admin.home'])->assignRole($role1);
-        Permission::create(['name'=>'admin.home'])->syncRoles([$role1,$role2,$role3,$role4]);
-        Permission::create(['name'=>'admin.users.index'])->syncRoles([$role1]);
-        Permission::create(['name'=>'admin.users.create'])->syncRoles([$role1]);
-        Permission::create(['name'=>'admin.users.update'])->syncRoles([$role1]);
-        
-        Permission::create(['name'=>'admin.categories.index'])->syncRoles([$role1, $role2]);
-        Permission::create(['name'=>'admin.categories.create'])->syncRoles([$role1]);
-        Permission::create(['name'=>'admin.categories.edit'])->syncRoles([$role1]);
-        Permission::create(['name'=>'admin.categories.destroy'])->syncRoles([$role1]);
+        $secretaria = Role::create(['name' => 'secretaria']);
+        $doctor = Role::create(['name' => 'doctor']);
+        $paciente = Role::create(['name' => 'paciente']);
+        $usuario = Role::create(['name' => 'usuario']);
 
-        Permission::create(['name'=>'admin.tags.index'])->syncRoles([$role1, $role2]);
-        Permission::create(['name'=>'admin.tags.create'])->syncRoles([$role1]);
-        Permission::create(['name'=>'admin.tags.edit'])->syncRoles([$role1]);
-        Permission::create(['name'=>'admin.tags.destroy'])->syncRoles([$role1]);
 
-        Permission::create(['name'=>'admin.posts.index'])->syncRoles([$role1,$role2]);
-        Permission::create(['name'=>'admin.posts.create'])->syncRoles([$role1,$role2]);
-        Permission::create(['name'=>'admin.posts.edit'])->syncRoles([$role1,$role2]);
-        Permission::create(['name'=>'admin.posts.destroy'])->syncRoles([$role1,$role2]);
-        // $role1->permissions()->attach();
 
-        //PICO Y PLACA
-        Permission::create(['name'=>'admin.vehiculos.pico_y_placa.index'])->syncRoles([$role1,$role3,$role4]);
-        Permission::create(['name'=>'admin.vehiculos.pico_y_placa.create'])->syncRoles([$role1]);
-        Permission::create(['name'=>'admin.vehiculos.pico_y_placa.update'])->syncRoles([$role1]);
-        //VEHICULOS
-        Permission::create(['name'=>'admin.vehiculos.index'])->syncRoles([$role1,$role3,$role4]);
-        Permission::create(['name'=>'admin.vehiculos.create'])->syncRoles([$role1]);
-        Permission::create(['name'=>'admin.vehiculos.update'])->syncRoles([$role1]);
-        //CURSOS
-        Permission::create(['name'=>'admin.cursos.index'])->syncRoles([$role1,$role3,$role4]);
-        Permission::create(['name'=>'admin.cursos.create'])->syncRoles([$role1]);
-        Permission::create(['name'=>'admin.cursos.update'])->syncRoles([$role1]);
-        //CLASES
-        Permission::create(['name'=>'admin.clases.index'])->syncRoles([$role1,$role3,$role4]);
-        Permission::create(['name'=>'admin.clases.create'])->syncRoles([$role1,$role3]);
-        Permission::create(['name'=>'admin.clases.update'])->syncRoles([$role1,$role3]);
+        // Permission::create(['name'=>'admin.home'])->assignRole($admin);
+        Permission::create(['name' => 'admin.home'])->syncRoles([$admin, $blogger, $role3, $role4]);
+        Permission::create(['name' => 'admin.users.index'])->syncRoles([$admin]);
+        Permission::create(['name' => 'admin.users.create'])->syncRoles([$admin]);
+        Permission::create(['name' => 'admin.users.update'])->syncRoles([$admin]);
+
+        Permission::create(['name' => 'admin.categories.index'])->syncRoles([$admin, $blogger]);
+        Permission::create(['name' => 'admin.categories.create'])->syncRoles([$admin]);
+        Permission::create(['name' => 'admin.categories.edit'])->syncRoles([$admin]);
+        Permission::create(['name' => 'admin.categories.destroy'])->syncRoles([$admin]);
+
+        Permission::create(['name' => 'admin.tags.index'])->syncRoles([$admin, $blogger]);
+        Permission::create(['name' => 'admin.tags.create'])->syncRoles([$admin]);
+        Permission::create(['name' => 'admin.tags.edit'])->syncRoles([$admin]);
+        Permission::create(['name' => 'admin.tags.destroy'])->syncRoles([$admin]);
+
+        Permission::create(['name' => 'admin.posts.index'])->syncRoles([$admin, $blogger]);
+        Permission::create(['name' => 'admin.posts.create'])->syncRoles([$admin, $blogger]);
+        Permission::create(['name' => 'admin.posts.edit'])->syncRoles([$admin, $blogger]);
+        Permission::create(['name' => 'admin.posts.destroy'])->syncRoles([$admin, $blogger]);
+
+        //------------------------[ ALEJANDRO PROJECT  ]---------------------------------
+        // Permission::create(['name'=>'admin.home'])->assignRole($admin);
+        Permission::create(['name' => 'admin.index']);
+        //rutas para el admin
+        Permission::create(['name' => 'admin.usuarios.index'])->syncRoles([$admin]);
+        Permission::create(['name' => 'admin.usuarios.create'])->syncRoles([$admin]);
+        Permission::create(['name' => 'admin.usuarios.store'])->syncRoles([$admin]);
+        Permission::create(['name' => 'admin.usuarios.show'])->syncRoles([$admin]);
+        Permission::create(['name' => 'admin.usuarios.edit'])->syncRoles([$admin]);
+        Permission::create(['name' => 'admin.usuarios.destroy'])->syncRoles([$admin]);
+        //rutas para el admin - secretarias
+        Permission::create(['name' => 'admin.secretarias.index'])->syncRoles([$admin]);
+        Permission::create(['name' => 'admin.secretarias.create'])->syncRoles([$admin]);
+        Permission::create(['name' => 'admin.secretarias.store'])->syncRoles([$admin]);
+        Permission::create(['name' => 'admin.secretarias.show'])->syncRoles([$admin]);
+        Permission::create(['name' => 'admin.secretarias.edit'])->syncRoles([$admin]);
+        Permission::create(['name' => 'admin.secretarias.destroy'])->syncRoles([$admin]);
+        //rutas para el admin - pacientes
+        Permission::create(['name' => 'admin.pacientes.index'])->syncRoles([$admin, $secretaria]);
+        Permission::create(['name' => 'admin.pacientes.create'])->syncRoles([$admin, $secretaria]);
+        Permission::create(['name' => 'admin.pacientes.store'])->syncRoles([$admin, $secretaria]);
+        Permission::create(['name' => 'admin.pacientes.show'])->syncRoles([$admin, $secretaria]);
+        Permission::create(['name' => 'admin.pacientes.edit'])->syncRoles([$admin, $secretaria]);
+        Permission::create(['name' => 'admin.pacientes.destroy'])->syncRoles([$admin, $secretaria]);
+        //rutas para el admin - consultorios
+        Permission::create(['name' => 'admin.consultorios.index'])->syncRoles([$admin, $secretaria]);
+        Permission::create(['name' => 'admin.consultorios.create'])->syncRoles([$admin, $secretaria]);
+        Permission::create(['name' => 'admin.consultorios.store'])->syncRoles([$admin, $secretaria]);
+        Permission::create(['name' => 'admin.consultorios.show'])->syncRoles([$admin, $secretaria]);
+        Permission::create(['name' => 'admin.consultorios.edit'])->syncRoles([$admin, $secretaria]);
+        Permission::create(['name' => 'admin.consultorios.destroy'])->syncRoles([$admin, $secretaria]);
+        //rutas para el admin - doctores
+        Permission::create(['name' => 'admin.doctores.index'])->syncRoles([$admin, $secretaria]);
+        Permission::create(['name' => 'admin.doctores.create'])->syncRoles([$admin, $secretaria]);
+        Permission::create(['name' => 'admin.doctores.store'])->syncRoles([$admin, $secretaria]);
+        Permission::create(['name' => 'admin.doctores.show'])->syncRoles([$admin, $secretaria]);
+        Permission::create(['name' => 'admin.doctores.edit'])->syncRoles([$admin, $secretaria]);
+        Permission::create(['name' => 'admin.doctores.destroy'])->syncRoles([$admin, $secretaria]);
+        //rutas para el admin - horarios
+        Permission::create(['name' => 'admin.horarios.index'])->syncRoles([$admin, $secretaria]);
+        Permission::create(['name' => 'admin.horarios.create'])->syncRoles([$admin, $secretaria]);
+        Permission::create(['name' => 'admin.horarios.store'])->syncRoles([$admin, $secretaria]);
+        Permission::create(['name' => 'admin.horarios.show'])->syncRoles([$admin, $secretaria]);
+        Permission::create(['name' => 'admin.horarios.edit'])->syncRoles([$admin, $secretaria]);
+        Permission::create(['name' => 'admin.horarios.destroy'])->syncRoles([$admin, $secretaria]);
+        //----------------------------------------------------------------------------------------
+        // $admin->permissions()->attach();
+
+        // //VEHICULOS
+        // Permission::create(['name'=>'admin.vehiculos.index'])->syncRoles([$admin,$role3,$role4]);
+        // Permission::create(['name'=>'admin.vehiculos.create'])->syncRoles([$admin]);
+        // Permission::create(['name'=>'admin.vehiculos.update'])->syncRoles([$admin]);  
+
+        // //PICO Y PLACA
+        // Permission::create(['name'=>'admin.vehiculos.pico_y_placa.index'])->syncRoles([$admin,$role3,$role4]);
+        // Permission::create(['name'=>'admin.vehiculos.pico_y_placa.create'])->syncRoles([$admin]);
+        // Permission::create(['name'=>'admin.vehiculos.pico_y_placa.update'])->syncRoles([$admin]);
+
+        // //CURSOS
+        // Permission::create(['name'=>'admin.cursos.index'])->syncRoles([$admin,$role3,$role4]);
+        // Permission::create(['name'=>'admin.cursos.create'])->syncRoles([$admin]);
+        // Permission::create(['name'=>'admin.cursos.update'])->syncRoles([$admin]);
+
+        // //CLASES
+        // Permission::create(['name'=>'admin.clases.index'])->syncRoles([$admin,$role3,$role4]);
+        // Permission::create(['name'=>'admin.clases.create'])->syncRoles([$admin,$role3]);
+        // Permission::create(['name'=>'admin.clases.update'])->syncRoles([$admin,$role3]);
 
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Consultorio;
+use App\Models\Doctor;
+use App\Models\Horario;
+use App\Models\Paciente;
+use App\Models\Secretaria;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,32 +18,182 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::create([
-            'name'=>'Jose Daniel Grijalba Osorio',
-            'sexo'=> 'M',
-            'telefono'=>'314852684',
-            'email'=> 'jose.jdgo97@gmail.com',
-            'email_verified_at' => now(),            
-            'password'=> bcrypt('123123123'),
-        ])->assignRole('Admin');
-        User::create([
-            'name'=>'Juan David Grijalba Osorio',
-            'sexo'=> 'M',
-            'telefono'=>'314852684',
-            'email'=> 'juandavidgo1997@gmail.com',
-            'email_verified_at' => now(),            
-            'password'=> bcrypt('123123123'),
-        ])->assignRole('Blogger');
+
+        User::factory(10)->create();
+
+        User::factory()->create([
+            'name' => 'Administrador',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('123123123'),
+        ])->assignRole('admin');
 
         User::create([
-            'name'=>'Hebron Customer',
-            'sexo'=> 'M',
-            'telefono'=>'314852684',
-            'email'=> 'hebron.customer@gmail.com',
-            'email_verified_at' => now(),            
-            'password'=> bcrypt('123123123'),
+            'name' => 'Jose Daniel Grijalba Osorio',
+            'email' => 'jose.jdgo97@gmail.com',
+            'password' => bcrypt('123123123'),
+        ])->assignRole('admin');
+
+        User::create([
+            'name' => 'Secretaria',
+            'email' => 'secretaria@gmail.com',
+            'password' => bcrypt('123123123'),
+        ])->assignRole('secretaria');
+
+        Secretaria::create([
+            'nombres' => 'Secretaria',
+            'apellidos' => 'Catrana',
+            'cc' => 'secretaria@gmail.com',
+            'celular' =>'3147078256',
+            'fecha_nacimiento' =>'10/10/2010',
+            'direccion' =>'calle 5 o este',
+            'user_id' =>'3',
         ]);
+        User::create([
+            'name' => 'Doctor',
+            'email' => 'doctor@gmail.com',
+            'password' => bcrypt('123123123'),
+        ])->assignRole('doctor');
 
-        User::factory(9)->create();
+        Doctor::create([
+            'nombres' => 'Doctor',
+            'apellidos' => 'Lewis',
+            'telefono' => '4564564565',
+            'licencia_medica' => '123123123',
+            'especialidad' => 'PEDIATRIA',
+            'user_id' =>'4',
+        ]);
+        User::create([
+            'name' => 'Doctor1',
+            'email' => 'doctor1@gmail.com',
+            'password' => bcrypt('123123123'),
+        ])->assignRole('doctor');
+
+        Doctor::create([
+            'nombres' => 'Doctor1',
+            'apellidos' => 'Lewis',
+            'telefono' => '432324324',
+            'licencia_medica' => '777777',
+            'especialidad' => 'ODONTOLOGIA',
+            'user_id' =>'5',
+        ]);
+        User::create([
+            'name' => 'Doctor2',
+            'email' => 'doctor2@gmail.com',
+            'password' => bcrypt('11111111'),
+        ])->assignRole('doctor');
+
+        Doctor::create([
+            'nombres' => 'Doctor2',
+            'apellidos' => 'Lewis',
+            'telefono' => '123123213',
+            'licencia_medica' => '222222',
+            'especialidad' => 'FISIOTERAPIA',
+            'user_id' =>'6',
+        ]);
+        User::create([
+            'name' => 'Paciente',
+            'email' => 'paciente@gmail.com',
+            'password' => bcrypt('123123123'),
+        ])->assignRole('paciente');
+        Consultorio::create([
+            'nombre' => 'PEDIATRIA',
+            'ubicacion' => '1-1A',
+            'capacidad' => '10',
+            'telefono' => '316548',
+            'especialidad' => 'PEDIATRIA',
+            'estado' => 'A',
+        ]);
+        Consultorio::create([
+            'nombre' => 'ODONTOLOGIA',
+            'ubicacion' => '1-1A',
+            'capacidad' => '10',
+            'telefono' => '316548',
+            'especialidad' => 'ODONTOLOGIA',
+            'estado' => 'A',
+        ]);
+        Consultorio::create([
+            'nombre' => 'pediatria',
+            'ubicacion' => '1-1A',
+            'capacidad' => '10',
+            'telefono' => '316548',
+            'especialidad' => 'pediatria',
+            'estado' => 'A',
+        ]);
+        Consultorio::create([
+            'nombre' => 'FISIOTERAPIA',
+            'ubicacion' => '1-1A',
+            'capacidad' => '10',
+            'telefono' => '316548',
+            'especialidad' => 'FISIOTERAPIA',
+            'estado' => 'A',
+        ]);
+        // Paciente::create([
+        //     'nombres' => 'Paciente',
+        //     'apellidos' => 'miania',
+        //     'cc' => '123166234' ,
+        //     'nro_seguro' => '45665645',
+        //     'fecha_nacimiento' => '02/05/2000',
+        //     'genero' => 'M',
+        //     'celular' => '324652344',
+        //     'correo' => 'paciente@gmail.com',
+        //     'direccion' => 'cre maga 15',
+        //     'alergias' => 'polvo',
+        //     'contacto_emergencia' => '366956215',
+        //     'user_id' =>'7',
+        // ]);
+        
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => bcrypt('123123123'),
+        ])->assignRole('usuario');
+        User::factory()->create([
+            'name' => 'user',
+            'email' => 'user@example.com',
+            'password' => bcrypt('123123123'),
+        ])->assignRole('usuario');
+
+        
+        Paciente::factory(10)->create()->each(function ($user){
+            $user->assignRole('paciente');
+        });
+        // $this->call(PacienteSeeder::class);
+
+        /// CREACION DE HORARIOS
+        Horario::create([
+            'dia' => 'LUNES',
+            'hora_inicio' => '8:00:00',
+            'hora_fin' => '14:00:00',
+            'doctor_id' => '1',
+            'consultorio_id' => '1',
+        ]);
+        //------------------ OLD DATA ----------------------------
+        // User::create([
+        //     'name'=>'Jose Daniel Grijalba Osorio',
+        //     'sexo'=> 'M',
+        //     'telefono'=>'314852684',
+        //     'email'=> 'jose.jdgo97@gmail.com',
+        //     'email_verified_at' => now(),            
+        //     'password'=> bcrypt('123123123'),
+        // ])->assignRole('Admin');
+        // User::create([
+        //     'name'=>'Juan David Grijalba Osorio',
+        //     'sexo'=> 'M',
+        //     'telefono'=>'314852684',
+        //     'email'=> 'juandavidgo1997@gmail.com',
+        //     'email_verified_at' => now(),            
+        //     'password'=> bcrypt('123123123'),
+        // ])->assignRole('Blogger');
+
+        // User::create([
+        //     'name'=>'Hebron Customer',
+        //     'sexo'=> 'M',
+        //     'telefono'=>'314852684',
+        //     'email'=> 'hebron.customer@gmail.com',
+        //     'email_verified_at' => now(),            
+        //     'password'=> bcrypt('123123123'),
+        // ]);
+
+        // User::factory(9)->create();
     }
 }
